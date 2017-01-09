2a1cca991aea31031649fa0ab280b70e20087b38
Steven Barnes <slbccfl@gmail.com>
2017-01-09 12:34:01 -0500

    mostly finished about and heaven buttons, starting to try jquery.load for html includes.

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-    $(".about-button").click(function() {
-    	$('.img-headshot').animate({left: '70%'},"slow","swing");
-    	$('.about-region').fadeIn("slow");
-    });
+	$('.img-headshot').fadeIn(2000).animate({left: '20%'},2000,"swing",function() {
+		$('.about-region').fadeIn("slow");
+	});
+	$('.heaven-button').click(function() {
+		$('.img-headshot, .about-region').fadeOut(1000, function() {
+			$('.broadwalk-background').fadeOut(1000);
+			$('.heaven-background, #heaven').fadeIn(4000, function() {
+				$('.heaven-background').fadeOut(2000);
+				$('.iowa-background, #heaven, #iowa, #aboutreturn').fadeIn(4000);
+			});
+		});
+	});
+	$('.about-button').click(function() {
+		$('.iowa-background, .heaven-background, #heaven, #iowa, #aboutreturn').fadeOut(1000);
+		$('.broadwalk-background, .img-headshot, .about-region').fadeIn(1000);
+	});
 });
+
