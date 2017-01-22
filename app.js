ccb66ab60a3bf1e9f7e46eaed05de705945f88e5
Steven Barnes <slbccfl@gmail.com>
2017-01-22 17:11:43 -0500

    web_projects page update not previously committed and fixes to resume and page switching

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 	$('.img-headshot').fadeIn(2000).animate({left: '20%'},2000,"swing",function() {
 		$('.about-region').fadeIn("slow");
-	});
-	$(".navbar").load("navbar.html", "html", function() {
 		$('.heaven-button').click(function() {
 			$('.img-headshot, .about-region').fadeOut(1000, function() {
 				$('.broadwalk-background').fadeOut(1000);
--- web_projects.js
+++ web_projects.js
@@ -1,34 +1,12 @@
 $(document).ready(function() {
 	$(".navbar").load("navbar.html", "html", function() {
-		$('.heaven-button').click(function() {
-			$('.img-headshot, .about-region').fadeOut(1000, function() {
-				$('.broadwalk-background').fadeOut(1000);
-				$('.heaven-background, #heaven').fadeIn(4000, function() {
-					$('.heaven-background').fadeOut(2000);
-					$('.iowa-background, #heaven, #iowa, #aboutreturn').fadeIn(4000);
-				});
-			});
-		});
-		$('.about-button').click(function() {
-			$('.iowa-background, .heaven-background, #heaven, #iowa, #aboutreturn, .resume-object').fadeOut(1000);
-			$('.broadwalk-background, .img-headshot, .about-region').fadeIn(1000);
-		});
-		$('.resume-button').click(function() {
-			$('.iowa-background, .heaven-background, #heaven, #iowa, #aboutreturn').fadeOut(1000);
-			$('.broadwalk-background, .img-headshot, .about-region').fadeOut(1000);
-			$('.resume-object').fadeIn(1000);
-		});
-		$('.web-projects-button').click(function() {
-			$(".navbar").load("navbar.html", "html", function() {
-				projectsText = localStorage.getItem("web_projects.json");
-				console.log(projectsText);
-				// projectsObject = JSON.parse(projectsText);
-				// $('.web_project_cell:first').load("web_project_cell", "html", function() {
-				// 	$('.web_project_cell:nth-child(1) > h2').html(projectsObject.length);
-				// });
-			});
-		});
-	}); 
+		projectsText = localStorage.getItem("web_projects.json");
+		console.log(projectsText);
+		// projectsObject = JSON.parse(projectsText);
+		// $('.web_project_cell:first').load("web_project_cell", "html", function() {
+		// 	$('.web_project_cell:nth-child(1) > h2').html(projectsObject.length);
+		// });
+	});
 });
 
 
