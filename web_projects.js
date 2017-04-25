75e54f1ba6227acffd36172735fd10bf3ea0458f
Steven Barnes <slbccfl@gmail.com>
2017-04-25 12:31:35 -0400

    Finally getting web_projects.json read correctly. Seems to be reading in the web_projects_cell.html overlay but is causing a jquery hairball after that.

--- web_projects.js
+++ web_projects.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
 	$(".navbar").load("navbar.html", function() {
-		projectsText = localStorage.getItem("web_projects.json");
-		console.log(projectsText);
-		// projectsObject = JSON.parse(projectsText);
-		// $('.web_project_cell:first').load("web_project_cell", "html", function() {
-		// 	$('.web_project_cell:nth-child(1) > h2').html(projectsObject.length);
-		// });
+		$.getJSON( "web_projects.json", function( projectsObject ) {
+			$('.web_project_cell:first').load("web_project_cell", "html", function() {
+				$('.web_project_cell:nth-child(1) > h2').html(projectsObject.length);
+			});
+		});
 	});
 });
