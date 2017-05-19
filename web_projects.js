f00e3aa86b569ce6e40a1d9e85c866033ee272b4
Steven Barnes <slbccfl@gmail.com>
2017-05-19 12:19:23 -0400

    progress on web_projects page. Now loading json and html fragments. Still working on processing the json content.

--- web_projects.js
+++ web_projects.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
 	$("#navbar").load("navbar.html", function() {
-		$.getJSON( "web_projects.json", function( projectsObject ) {
+		console.log("navbar.html loaded to #navbar");
+		$.getJSON( "web_projects.json", function( projectsArray ) {
+			console.log("web_projects.json loaded to projectsObject");
+			console.log(projectsArray);
 			$('.web_project_cell').load("web_project_cell.html", function() {
-				for (i in projectsObject) {
-					var project = projectsObject[i];
-					$(".web_projects_cell:nth-child(i) h2").replaceWith(project.appTitle);
-					$(".web_projects_cell:nth-child(i) .web_work_about").replaceWith(project.appLongDesc);
+				console.log("web_project_cell.html loaded to .web_project_cell");
 
-				};
+			});
+			$.each(projectsArray.projects, function (i, project) {
+				console.log("project " + i + " assigned to var project");
+				console.log(project.appTitle);
+				console.log(project.appLongDesc);
+				// $(".web_projects_cell:nth-child(i) h2").replaceWith(project.appTitle);
+				// $(".web_projects_cell:nth-child(i) .web_work_about").replaceWith(project.appLongDesc);
+
 			});
 		});
 	});
