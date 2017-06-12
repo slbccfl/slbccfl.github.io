0fc8a394dff94328717be533274b6647948039d1
Steven Barnes <slbccfl@gmail.com>
2017-06-12 13:53:07 -0400

    minor progress on loading web_project_cell

--- web_projects.js
+++ web_projects.js
@@ -2,20 +2,19 @@ $(document).ready(function() {
 	$("#navbar").load("navbar.html", function() {
 		console.log("navbar.html loaded to #navbar");
 		$.getJSON( "web_projects.json", function( projectsArray ) {
-			console.log("web_projects.json loaded to projectsObject");
+			console.log("web_projects.json loaded to projectsArray");
 			console.log(projectsArray);
-			$('.web_project_cell').load("web_project_cell.html", function() {
-				console.log("web_project_cell.html loaded to .web_project_cell");
-
-			});
 			$.each(projectsArray.projects, function (i, project) {
-				console.log("project " + i + " assigned to project");
-				console.log(project.title);
-				console.log(project.longDesc);
-				$(".web_project_cell:nth-child(" + i + ") h2").html(project.title);
-				$(".web_project_cell:nth-child(" + i + ") .web_work_about").html(project.longDesc);
-				$(".web_project_cell:nth-child(" + i + ") img").attr("src", project.image);
+				$(".web_project_cell:nth-child(" + i + ")").load("web_project_cell.html", function() {
+					console.log("web_project_cell.html loaded to .web_project_cell " + i);
+					console.log("project " + i + " assigned to project");
+					console.log(project.title);
+					console.log(project.longDesc);
+					$(".table .web_project_cell:nth-child(" + i + ") h2").html(project.title);
+					$(".table .web_project_cell:nth-child(" + i + ") .web_work_about").html(project.longDesc);
+					$(".table .web_project_cell:nth-child(" + i + ") img").attr("src", project.image);
 
+				});
 			});
 		});
 	});
