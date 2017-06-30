56de24360f6186b320d31d0f571080f59b224180
Steven Barnes <slbccfl@gmail.com>
2017-06-30 15:04:00 -0400

    Finally got only the web_projects needed to load. Progressgit add .

--- web_projects.js
+++ web_projects.js
@@ -5,15 +5,14 @@ $(document).ready(function() {
 			console.log("web_projects.json loaded to projectsArray");
 			console.log(projectsArray);
 			$.each(projectsArray.projects, function (i, project) {
-				$(".web_project_cell:nth-child(" + i + ")").load("web_project_cell.html", function() {
+				console.log("project " + i + " assigned to project");
+				console.log(project.title);
+				console.log(project.longDesc);
+				$("table td:eq(" + i + ")").load("web_project_cell.html", function() {
 					console.log("web_project_cell.html loaded to .web_project_cell " + i);
-					console.log("project " + i + " assigned to project");
-					console.log(project.title);
-					console.log(project.longDesc);
-					$(".table .web_project_cell:nth-child(" + i + ") h2").html(project.title);
-					$(".table .web_project_cell:nth-child(" + i + ") .web_work_about").html(project.longDesc);
-					$(".table .web_project_cell:nth-child(" + i + ") img").attr("src", project.image);
-
+					$("table td:eq(" + i + ") h2").html(project.title);
+					$("table td:eq(" + i + ") .web_work_about").html(project.longDesc);
+					$("table td:eq(" + i + ") img").attr("src", project.image);
 				});
 			});
 		});
